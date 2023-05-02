3f7baacd76b25f5993847869de6183fa12e4fdbd
EeZeeRo <78751696+EeZeeRo@users.noreply.github.com>
2023-05-02 19:00:52 +0700

    Delete pharm directory

--- pharm/src/hooks/useAuth.js
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
-
-export function useAuth() {
-    const { user, loading, error, signInWithEmailAndPassword, signOut } =
-        useContext(AuthContext);
-    return { user, loading, error, signInWithEmailAndPassword, signOut };
-}
